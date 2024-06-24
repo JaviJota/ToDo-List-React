a92216e09cf5a43a760a26352f02bbfe5e4df7ea
JaviJota <165896246+JaviJota@users.noreply.github.com>
2024-06-24 17:48:56 +0000

    Crear ToDo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,46 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import { FaTrash } from "react-icons/fa";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="container">
+      <h1>My To-Do List</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            placeholder="Add new task..."
+            onChange={(e) => setInputValue(e.target.value)}
+            onKeyUp={(e) => {
+              if (e.key === "Enter" && inputValue.length > 0) {
+                setTodos(
+                  todos.concat(
+                    inputValue.charAt(0).toUpperCase() + inputValue.slice(1)
+                  )
+                );
+                setInputValue("");
+              }
+            }}
+            value={inputValue}
+          />
+        </li>
+        {todos.map((todo, index) => (
+          <li>
+            {todo}{" "}
+            <FaTrash
+              /* Eliminar list item al hacer click sobre el icono */
+              onClick={() =>
+                setTodos(todos.filter((todo, todoIndex) => index != todoIndex))
+              }
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
